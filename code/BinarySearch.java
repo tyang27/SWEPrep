8fea084fe11e09750eed520738c86a876d87befa
Tony Yang <tonydmyang@gmail.com>
2019-08-11 23:10:50 -0400

    fix typo, add floor/ceiling

--- code/BinarySearch.java
+++ code/BinarySearch.java
@@ -4,23 +4,33 @@ class BinarySearch {
 
   public static int mySearch(int[] A, int key, int low, int high) {
     if (high < low) {
+      //return low; // ceiling
+      //return high; // floor
       return -1;
     }
     int mid = low + (high - low)/2;
-    if (A[mid] == key)
+    if (A[mid] == key) {
       return mid;
-    else if (A[mid] < key)
+    } else if (A[mid] > key) {
       return mySearch(A, key, low, mid-1);
-    else
+    } else {
       return mySearch(A, key, mid+1, high);
+    }
+  }
+
+  public static int mySearch1(int[] A, int key, int low, int high) {
+    return -1;
   }
 
   public static void main(String[] args) {
     int[] arr = {1, 4, 9, 16, 25, 36};
-    int index = mySearch(arr, 9, 0, arr.length-1);
+    int index;
+    index = mySearch(arr, 9, 0, arr.length-1);
     assert index == 2;
     index = mySearch(arr, 36, 0, arr.length-1);
     assert index == 5;
+    index = mySearch(arr, 25, 0, arr.length-1);
+    assert index == 4;
     index = mySearch(arr, 1, 0, arr.length-1);
     assert index == 0;
     index = mySearch(arr, 26, 0, arr.length-1);
